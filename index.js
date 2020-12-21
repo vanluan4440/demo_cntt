e3cc14c19851c5990ac9b627ad4d85831f94b659
vanluan4440 <60852744+vanluan4440@users.noreply.github.com>
2020-12-22 00:20:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,45 +3,48 @@ function time(){
     gio = date.getHours()
     phut = date.getMinutes()
     if (phut < 10){
-        document.getElementById('time').innerHTML ='Timer: ' + gio + ':'+"0"+ phut
+        return gio + ':'+"0"+ phut
     }
     else if (phut > 10){
-        document.getElementById('time').innerHTML ='Timer: ' + gio + ':'+ phut
+        return  gio + ':'+ phut
     } 
 }
-setInterval(time,1000)
 function day(){
     switch (new Date().getDay()) {
         case 0:
-          document.getElementById('date').innerHTML =  "Today is: "+ "Sunday"
+          return "Sunday";
           break;
         case 1:
-            document.getElementById('date').innerHTML = "Today is: "+"Monday";
+          return "Monday";
           break;
         case 2:
-            document.getElementById('date').innerHTML = "Today is: "+"Tuesday";
+          return "Tuesday";
           break;
         case 3:
-            document.getElementById('date').innerHTML ="Today is: "+ "Wednesday";
+          return "Wednesday";
           break;
         case 4:
-            document.getElementById('date').innerHTML = "Today is: "+"Thursday";
+          return "Thursday";
           break;
         case 5:
-            document.getElementById('date').innerHTML = "Today is: "+"Friday";
+          return "Friday";
           break;
         case 6:
-            document.getElementById('date').innerHTML = "Today is: "+"Saturday";
+          return "Saturday";
       }
+      
 }
-setInterval(day,1000)
+
 function date(){
     var date = new Date()
     var time = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear()
-    document.getElementById('sub_date').innerHTML ="Date: "+ time
+    return time
 }
-setInterval(date,1000)
 
+function index_time(){
+  document.getElementById("sub_date").innerHTML = day() +"--"+ date() +", Now: "+ time()
+}
+setInterval(index_time,1000)
 function index(){
   pass = document.getElementById("pass").value
   if (pass == "itc.edu.vn"){
